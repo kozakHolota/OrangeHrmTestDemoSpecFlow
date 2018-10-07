3cd1fae19e14833766ba1d3078702efb49edc0e8
Pavlo Mryhlotskyi <kozak.holota@gmail.com>
2018-10-07 22:15:15 +0300

    Created the project skeleton.

---
+++ OrangeHrmTestDemoSpecFlow/OrangeHrmTestDemoSpecFlow/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OrangeHrmTestDemoSpecFlow")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OrangeHrmTestDemoSpecFlow")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4cd3c104-52a8-42b1-860e-ae3b38875d72")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
